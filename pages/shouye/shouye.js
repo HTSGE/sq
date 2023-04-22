5d9ef3d2bfe4eb6e5f66b6d057837863c659ccee
OER <110009644+HTSGE@users.noreply.github.com>
2023-04-22 22:29:44 +0800

    Add files via upload

--- pages/shouye/shouye.js
+++ pages/shouye/shouye.js
@@ -1,21 +1,27 @@
-onClick(){window.open('toutu','_blank');}
+onClick(){window.open('tt','_blank');}
 
-onClick_1(){window.open('dongtai','_blank');}
+onClick_1(){window.open('dt','_blank');}
 
-function onClick_2(){location.href = '../wenzhangliebiao/wenzhangliebiao.html';}
+onClick_2(){window.open('gg','_blank');}
 
-onClick_3(){window.open('shipin','_blank');}
+onClick_3(){window.open('vdo','_blank');}
 
-onClick_4(){window.open('peiyin','_blank');}
+onClick_4(){window.open('vic','_blank');}
 
-onClick_5(){window.open('huodong','_blank');}
+onClick_5(){window.open('fl','_blank');}
 
-onClick_6(){window.open('fuli','_blank');}
+onClick_6(){window.open('wj','_blank');}
 
-onClick_7(){window.open('wenjvan','_blank');}
+onClick_7(){window.open('fk','_blank');}
 
-onClick_8(){window.open('https://support.qq.com/product/564428','_blank');}
+onClick_8(){window.open('qq','_blank');}
 
-onClick_9(){window.open('aboutus','_blank');}
+onClick_9(){window.open('zj1','_blank');}
 
-onClick_10(){window.open('sponsorus','_blank');}
+onClick_10(){window.open('zj2','_blank');}
+
+onClick_11(){window.open('zj3','_blank');}
+
+onClick_12(){window.open('abt','_blank');}
+
+onClick_13(){window.open('sps','_blank');}
