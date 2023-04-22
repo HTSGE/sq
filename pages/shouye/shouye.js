172ecf55189db3b4976c2c64910451b619f999c5
OER <110009644+HTSGE@users.noreply.github.com>
2023-04-22 20:16:43 +0800

    Add files via upload

---
+++ pages/shouye/shouye.js
@@ -0,0 +1,21 @@
+onClick(){window.open('toutu','_blank');}
+
+onClick_1(){window.open('dongtai','_blank');}
+
+function onClick_2(){location.href = '../wenzhangliebiao/wenzhangliebiao.html';}
+
+onClick_3(){window.open('shipin','_blank');}
+
+onClick_4(){window.open('peiyin','_blank');}
+
+onClick_5(){window.open('huodong','_blank');}
+
+onClick_6(){window.open('fuli','_blank');}
+
+onClick_7(){window.open('wenjvan','_blank');}
+
+onClick_8(){window.open('https://support.qq.com/product/564428','_blank');}
+
+onClick_9(){window.open('aboutus','_blank');}
+
+onClick_10(){window.open('sponsorus','_blank');}
